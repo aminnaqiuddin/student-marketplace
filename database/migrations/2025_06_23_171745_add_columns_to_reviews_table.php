75cd0b7a7e15fdd7a8555bad46ee184864273ae3
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-04 03:24:27 +0800

    Fix final migration files

---
+++ database/migrations/2025_06_23_171745_add_columns_to_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_id')->after('id');
+            $table->unsignedBigInteger('user_id')->after('product_id');
+            $table->tinyInteger('rating')->after('user_id');
+            $table->text('comment')->nullable()->after('rating');
+
+            // Optional: Add foreign keys
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            //
+        });
+    }
+};
