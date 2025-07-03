69a5a2e7dac2a7a8b33b607fb700c8bcfad4846b
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-04 04:04:34 +0800

    Fix down method in update_users_table migration

--- database/migrations/2025_06_25_162337_update_users_table_add_role_column.php
+++ database/migrations/2025_06_25_162337_update_users_table_add_role_column.php
@@ -21,7 +21,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id')->nullable();
+            //$table->unsignedBigInteger('role_id')->nullable();
             $table->dropColumn('role');
         });
     }
