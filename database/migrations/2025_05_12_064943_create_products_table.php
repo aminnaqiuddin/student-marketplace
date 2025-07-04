c0f8249a87ededd8edbaea4cc1fa0203ee7089ef
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-05 06:44:15 +0800

    Add category_id to products table migration

--- database/migrations/2025_05_12_064943_create_products_table.php
+++ database/migrations/2025_05_12_064943_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->string('title');
             $table->text('description');
             $table->decimal('price', 8, 2);
