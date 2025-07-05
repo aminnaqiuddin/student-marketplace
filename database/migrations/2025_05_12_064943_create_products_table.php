113bf3b94dc52df2dfa082a661b6041d21e19ee6
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-05 15:37:08 +0800

    fix migration

--- database/migrations/2025_05_12_064943_create_products_table.php
+++ database/migrations/2025_05_12_064943_create_products_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
             $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
             $table->string('title');
             $table->text('description');
             $table->decimal('price', 8, 2);
