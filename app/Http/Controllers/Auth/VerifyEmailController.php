761884996bb33da584dd7cb3fcacc32f3709efe4
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-06 00:45:13 +0800

    Fix email verification

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
 
 class VerifyEmailController extends Controller
 {
@@ -15,14 +16,21 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
+        Log::info('Verification request started for user: ' . $request->user()->id);
+
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+            Log::info('User ' . $request->user()->id . ' was already verified.');
+            return redirect()->intended(config('fortify.home').'?verified=1');
         }
 
         if ($request->user()->markEmailAsVerified()) {
+            Log::info('markEmailAsVerified was successful for user: ' . $request->user()->id);
             event(new Verified($request->user()));
+        } else {
+            Log::error('markEmailAsVerified FAILED for user: ' . $request->user()->id);
         }
 
-        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+        Log::info('Verification request finished. Redirecting user: ' . $request->user()->id);
+        return redirect()->intended(config('fortify.home').'?verified=1');
     }
 }
