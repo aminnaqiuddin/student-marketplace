a6cf67d993de947da024d2ef089230fc32df6fdd
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-06 14:50:25 +0800

    Fix footer links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,18 +99,18 @@
                 <div class="min-h-[260px]">
                     <h5 class="font-bold text-lg mb-3">Categories</h5>
                     <ul class="space-y-2 text-sm">
-                        <li><a href="#" class="hover:text-uitm-gold">Mens' Fashion</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Women's Fashion</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Books & Stationery</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Laptops & Accessories</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Mobile Phones & Gadgets</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Sports & Outdoors</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Services</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Video Games & Consoles</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Audio Equipment</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Photography</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Food & Beverages</a></li>
-                        <li><a href="#" class="hover:text-uitm-gold">Event Tickets</a></li>
+                        <li><span class="text-gray-700">Mens' Fashion</span></li>
+                        <li><span class="text-gray-700">Women's Fashion</span></li>
+                        <li><span class="text-gray-700">Books & Stationery</span></li>
+                        <li><span class="text-gray-700">Laptops & Accessories</span></li>
+                        <li><span class="text-gray-700">Mobile Phones & Gadgets</span></li>
+                        <li><span class="text-gray-700">Sports & Outdoors</span></li>
+                        <li><span class="text-gray-700">Services</span></li>
+                        <li><span class="text-gray-700">Video Games & Consoles</span></li>
+                        <li><span class="text-gray-700">Audio Equipment</span></li>
+                        <li><span class="text-gray-700">Photography</span></li>
+                        <li><span class="text-gray-700">Food & Beverages</span></li>
+                        <li><span class="text-gray-700">Event Tickets</span></li>
                     </ul>
                 </div>
 
