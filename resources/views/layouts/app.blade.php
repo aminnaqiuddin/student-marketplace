f2dc67aca6a4cfcd2557281fec4ae343fc92d99b
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-06 15:33:36 +0800

    Adding Chatbot for FAQs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,6 +90,9 @@
         @endif
     </script>
 
+    <script>
+        (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="Bq2idzuux9v1CwQ9deQd0";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+    </script>
 
     <!-- Footer -->
     <footer class="bg-uitm-purple text-white border-t border-uitm-gold">
