df0ed161035bf1fc8dc85338c0047ce17acbdf75
Amin Naqiuddin <aminnaqiuddin98@gmail.com>
2025-07-09 00:56:10 +0800

    Add admin route test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ProductReportController;
 use Illuminate\Http\Request;
 
+Route::get('/admin', function() {
+    dd('The /admin route is being hit correctly.');
+});
+
 // ==================== Stripe Webhook ====================
 Route::post('/stripe/webhook', [StripeWebhookController::class, 'handleWebhook'])
     ->name('stripe.webhook')
